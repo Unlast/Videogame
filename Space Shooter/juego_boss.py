51be6548337a54e87f4da3e3af2640991bd4cd06
Unlast <54677163+Unlast@users.noreply.github.com>
2019-11-23 11:14:19 -0300

    Add files via upload

--- Space Shooter/juego_boss.py
+++ Space Shooter/juego_boss.py
@@ -19,24 +19,24 @@ def cargarEnemigos():
        #el tercer espacio es la distancia que recorre sobre el eje x
         posx = 45
         for x in range(1,6):
-            armada = Enemigo.nave_enemiga(posx,150,40, 'recursos/imagenes/enemigo003.bmp', 'recursos/imagenes/enemigo004.bmp',3) #Segundo valor distancia en y
+            armada = Enemigo.jefe_enemigo(posx,150,40, 'recursos/imagenes/enemigo003.bmp', 'recursos/imagenes/enemigo004.bmp') #Segundo valor distancia en y
             LISTA_ENEMIGOS.append(armada)
             posx = posx + 50 
        
         
         posx = 90
         for x in range(1,4):
-            armada = Enemigo.nave_enemiga(posx,100,80, 'recursos/imagenes/enemigo001.bmp', 'recursos/imagenes/enemigo002.bmp')
+            armada = Enemigo.jefe_enemigo(posx,100,80, 'recursos/imagenes/enemigo003.bmp', 'recursos/imagenes/enemigo004.bmp') #Segundo valor distancia en y
             LISTA_ENEMIGOS.append(armada)
             posx = posx + 75
        
         
         posx = 180 #Desplazamiento en x
         for x in range(1,2):
-            armada = Enemigo.nave_enemiga(posx,50,120, 'recursos/imagenes/enemigo001.bmp', 'recursos/imagenes/enemigo002.bmp') #Segundo valor distancia en y
+            armada = Enemigo.jefe_enemigo(posx,50,120, 'recursos/imagenes/enemigo003.bmp', 'recursos/imagenes/enemigo004.bmp') #Segundo valor distancia en y
             LISTA_ENEMIGOS.append(armada)
             posx = posx + 50
-        
+            
 def detenerEnemigo():
     for armada in LISTA_ENEMIGOS:
         for disparo in armada.listaTiro:
