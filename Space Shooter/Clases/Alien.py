3f2cbc54299980c2b6ae0ac2fc1edc3d05df9f9e
Unlast <54677163+Unlast@users.noreply.github.com>
2019-11-23 11:13:55 -0300

    Add files via upload

--- Space Shooter/Clases/Alien.py
+++ Space Shooter/Clases/Alien.py
@@ -2,6 +2,7 @@ import pygame,os
 from pygame.locals import *
 from Clases import Proyectil
 from random import randint
+from Clases import NaveEspacial
 
 ANCHO = 360
 ALTO = 740
@@ -48,29 +49,29 @@ class nave_enemiga(pygame.sprite.Sprite):
     def animacion(self,tiempo):
         
         if self.conquista == False:
-            self.__movimientos()
+            self.movimientos()
 
-            self.__ataque()
+            self.ataque()
     
             if  self.posicionImagen>=len(self.listaImagenes)-1:
                 self.posicionImagen = 0
             else:
                 self.posicionImagen +=1
     
-    def __movimientos(self):
+    def movimientos(self):
         if self.contador <3:
-            self.__movimientoLateral()
+            self.movimientoLateral()
         else:
-            self.__descenso()
+            self.descenso()
     
-    def __descenso(self):
+    def descenso(self):
         if self.maxDescenso == self.rect.top:
             self.contador =0
             self.maxDescenso = self.rect.top + 40
         else:
             self.rect.top +=20
    
-    def __movimientoLateral(self):
+    def movimientoLateral(self):
         if self.derecha == True:
             self.rect.left = self.rect.left+ self.velocidad
             if self.rect.left > self.limiteDer:
@@ -81,13 +82,77 @@ class nave_enemiga(pygame.sprite.Sprite):
             if self.rect.left <self.limiteIzq:
                 self.derecha = True
                 
-    def __ataque(self):
+    def ataque(self):
         if (randint(0,600)<self.rangoDisparo):
-            self.__disparo()
+            self.disparo()
 
-    def __disparo(self):
+    def disparo(self):
         x,y = self.rect.center
         miProyectil = Proyectil.Proyectil(x,y,'recursos/imagenes/tiro2.bmp',False)
         self.listaTiro.append(miProyectil)
         self.sonidoDisparo.play()
  
+
+class jefe_enemigo(nave_enemiga):
+    def __init__(self,posx,posy, distancia, imagenUno, imagenDos):
+        self.posx = posx
+        self.posy = posy
+        self.imagenA = pygame.image.load(imagenUno)
+        self.imagenB = pygame.image.load(imagenDos)
+        self.listaImagenes = [self.imagenA, self.imagenB]
+
+        self.distancia = distancia
+        self.posicionImagen = 0
+        self.imagenEnemigo = self.listaImagenes[self.posicionImagen]
+
+        self.colision = 0
+        self.vivo = True
+        self.rect = self.imagenEnemigo.get_rect()
+        self.sonidoDisparo = pygame.mixer.Sound('recursos/audio/pium.wav')
+    
+        self.listaTiro = []
+        self.velocidadTiro = 450
+        self.velocidad = 1
+        self.rect.top = posy
+        self.rect.left = posx
+        
+        self.cambioImagen = 1
+    	
+        self.derecha = True
+        self.contador = 0
+        self.maxDescenso = self.rect.top+40
+        
+        self.limiteDer= posx + distancia
+        self.limiteIzq= posx - distancia
+    	
+        self.rangoDisparo = 1
+        self.conquista = False 
+
+        self.conquista = False
+    def mostrar(self, superficie):
+        nave_enemiga.mostrar(self,superficie)
+    
+    def animacion(self, tiempo):
+        nave_enemiga.animacion(self,tiempo)
+    
+    def movimientos(self):
+        nave_enemiga.movimientos(self)
+       
+    def descenso(self):
+        nave_enemiga.descenso(self)
+    
+    def movimientoLateral(self):
+        nave_enemiga.movimientoLateral(self)
+    
+    def ataque(self):
+        nave_enemiga.ataque(self)
+    
+    def disparo(self):
+        x,y = self.rect.center
+        miProyectil = Proyectil.Proyectil(x,y,'recursos/imagenes/Proyectil.png',False)
+        self.listaTiro.append(miProyectil)
+        self.sonidoDisparo.play()
+    
+    
+            
+            
