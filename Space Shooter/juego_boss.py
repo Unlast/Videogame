42e18bf1af9cf64bcd965fdbc1cc287fac8a665d
Unlast <54677163+Unlast@users.noreply.github.com>
2019-11-27 16:03:06 -0300

    Add files via upload

--- Space Shooter/juego_boss.py
+++ Space Shooter/juego_boss.py
@@ -12,27 +12,27 @@ from Clases import Niveles
 ANCHO = 360
 ALTO = 740
 LISTA_ENEMIGOS = []
+LISTA_ENEMIGOS2 = []
+
 NEGRO = (0, 0, 0)
 
 def NuevoJuego():
     pygame.init()
     ventana = pygame.display.set_mode((ANCHO,ALTO))
-    pygame.display.set_caption("Space Shoot")
     imagenFondo = pygame.image.load(os.path.join('recursos','imagenes','fondo.jpg'))
-
+    Niveles.pantalla_juego(ventana, ANCHO, ALTO)
     fuente = pygame.font.Font("recursos/fuentes//monolight.ttf", 30)
-    textoVictoria = fuente.render("Todavía hay maaas",45,(120,100,40))
+    textoVictoria = fuente.render("Todavia hay mas",45,(120,100,40))
     textoDerrota = fuente.render("Game Over",75,(254,0,227))
     pygame.mixer.music.load('recursos/audio/menu.mp3')
     pygame.mixer.music.play(4)
     
-    Niveles.cargarEnemigos(LISTA_ENEMIGOS)
     jugador = NaveEspacial.nave()
     pygame.key.set_repeat(1)
     reloj = pygame.time.Clock()
     juego = True
     ventana.fill(NEGRO)
-    
+    Niveles.cargarEnemigosPiramide(LISTA_ENEMIGOS)
     while True:
 
         #Mayor es el tick, mayor es la velocidad de cambio
@@ -40,7 +40,7 @@ def NuevoJuego():
         tiempo = reloj
         ventana.blit(imagenFondo,(0,0))
         jugador.movimiento()
-        
+        pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -53,32 +53,35 @@ def NuevoJuego():
                                 
                     elif event.key == K_RIGHT:
                         jugador.rect.right += jugador.Velocidad
-                                
-                    elif event.key == K_s:
+                
+                if event.type == pygame.KEYUP:
+                    if event.key == K_s:
                         x,y = jugador.rect.center
                         jugador.disparar(x,y)                       
         ventana.blit(imagenFondo,(0,0))
         jugador.dibujar(ventana)
         
-        jugador.colision(LISTA_ENEMIGOS,ventana,Enemigo.nave_enemiga,3)
+        jugador.colision(LISTA_ENEMIGOS,ventana,Enemigo.nave_enemiga,0)
         Enemigo.nave_enemiga.colision(LISTA_ENEMIGOS, tiempo,ventana,jugador, ALTO, ANCHO, textoDerrota)
-        
+        jugador.colision(LISTA_ENEMIGOS,ventana,Enemigo.jefe_enemigo,4)
+
         if jugador.Vida == False:
             ventana.blit(textoDerrota,(ANCHO/2,ALTO/5))
+            
             
         if len(LISTA_ENEMIGOS) == 0 and jugador.Vida == True and jugador.nivel == False:
             time.sleep(3)
             jugador.nivel = True
-            Niveles.cargarBoss(LISTA_ENEMIGOS)
-            jugador.colision(LISTA_ENEMIGOS,ventana,Enemigo.nave_enemiga,9)
-            Enemigo.nave_enemiga.colision(LISTA_ENEMIGOS, tiempo,ventana,jugador, ALTO, ANCHO, textoDerrota)
-            if len(LISTA_ENEMIGOS) == 0 and jugador.Vida == True and jugador.nivel2 == False:
-                jugador.nivel2 = True
-        if jugador.nivel == True and jugador.nivel2 == True:
+            Niveles.pantalla_carga(ventana, ANCHO, ALTO)
             jugador.ganar()
-            ventana.blit(textoVictoria,(75,300))
+            Niveles.cargarBossPiramide(LISTA_ENEMIGOS)
+            Niveles.pantalla_juego(ventana,ANCHO, ALTO)    
+            if len(LISTA_ENEMIGOS) == 0 and len(LISTA_ENEMIGOS2)== 0 and jugador.Vida == True: 
+                jugador.ganar()
+                ventana.blit(textoVictoria,(ANCHO/2,ALTO/5))
+            
         pygame.display.update()
-    
+        
 def salir_del_programa():
     import sys
     sys.exit(0)
